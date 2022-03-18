a245b7271d708f53be7e1ac6b7ea33a38d69c317
Bergo <bergo@tutanota.com>
2022-03-18 12:51:13 +0200

    starting to add pygame

--- game/game.py
+++ game/game.py
@@ -34,51 +34,20 @@ def get_blind_position(button, players_length, sb=True):
         return button + 2
 
 
-# def get_winner(players):
-#     """
-#     Ac 2d 3h 4s
-#     """
-#     best_badugi = 0
-#     best_high = [14]
-#     winners = []
-#     for player in players:
-#         badugi_hand = []
-#         for card in player.hand:
-#             has_suit = card["suit"] in [bhc["suit"] for bhc in badugi_hand]
-#             has_number = card["number"] in [bhc["number"] for bhc in badugi_hand]
-#             if not has_suit and not has_number:
-#                 badugi_hand.insert(0, card)
-#         if len(badugi_hand) > best_badugi:
-#             winners = [player]
-#             best_badugi = len(badugi_hand)
-#             best_high = [bhc["number"] for bhc in badugi_hand]
-#         elif len(badugi_hand) == best_badugi:
-#             same_hand = True
-#             for i, card in enumerate(badugi_hand):
-#                 if card["number"] < best_high[i]:
-#                     winners = [player]
-#                     new_best_high = [bhc["number"] for bhc in badugi_hand]
-#                     best_high = new_best_high
-#                     same_hand = False
-#                     break
-#                 elif card["number"] > best_high[i]:
-#                     same_hand = False
-#                     break
-#             if same_hand:
-#                 winners.append(player)
-#     return winners
-
-
 class Badugi:
     """
     docstring for Badugi.
     """
 
-    # def __init__(self, window, width, height, players, starting_chips):
-    def __init__(self, players, starting_chips):
-        # self.window = window
-        # self.width = width
-        # self.height = height
+    SCORE_FONT = pygame.font.SysFont("comicsans", 50)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+
+    def __init__(self, window, width, height, players, starting_chips):
+        # def __init__(self, players, starting_chips):
+        self.window = window
+        self.width = width
+        self.height = height
         self.starting_chips = starting_chips
         self.players = create_players(players, starting_chips)
         self.button = randrange(0, len(players))
@@ -115,6 +84,8 @@ class Badugi:
             player.hand = player_hand
             player.hand.sort(key=lambda x: x["number"])
 
+        # Betting rounds
+
         # Find winner
         dealer.collect()
         winners = get_winners(self.players)
@@ -129,7 +100,7 @@ class Badugi:
 
 if __name__ == "__main__":
     players = ["Player1", "Player2"]
-    badugi = Badugi(players, 1000)
+    badugi = Badugi(players, 2000)
     for i in range(20000):
         badugi.deal_hand()
     for player in badugi.players:
